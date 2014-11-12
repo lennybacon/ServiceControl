3dc553e98289c9c8922dfa1de172ca26c3ad37a7
Daniel Fisher <info@lennybacon.com>
2014-11-12 14:05:18 +0100

    Don't create further exceptions when the bootstrapping failed

--- src/ServiceControl/Hosting/Host.cs
+++ src/ServiceControl/Hosting/Host.cs
@@ -26,8 +26,10 @@
 
         protected override void OnStop()
         {
-            bootstrapper.Stop();
-
+            if (bootstrapper != null)
+            {
+                bootstrapper.Stop();
+            }
             OnStopping();
         }
 
